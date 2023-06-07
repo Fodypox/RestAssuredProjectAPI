22de3f15b3e6d3a7776078c4171757b35a5244ae
user <sevgi012127@gmail>
2023-06-07 11:54:56 -0600

    I tested

---
+++ src/test/java/Campus/POJO_Models/Fees.java
@@ -0,0 +1,95 @@
+package Campus.POJO_Models;
+
+import java.util.ArrayList;
+
+public class Fees {
+
+    private  String id;
+    private String name;
+    private String code;
+    private String integrationCode;
+    private int priority;
+    private ArrayList<TranslateName> translateName;
+
+    private boolean active;
+
+    public String getId(String s) {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode(String s) {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getIntegrationCode(String memi) {
+        return integrationCode;
+    }
+
+    public void setIntegrationCode(String integrationCode) {
+        this.integrationCode = integrationCode;
+    }
+
+    public boolean isActive(boolean b) {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getPriority(int i) {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getIntegrationCode() {
+        return integrationCode;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public ArrayList<TranslateName> getTranslateName() {
+        return translateName;
+    }
+
+    public void setTranslateName(ArrayList<TranslateName> translateName) {
+        this.translateName = translateName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+}
