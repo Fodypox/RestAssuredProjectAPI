019ac3a8a644cb0c2fe7aa3852669df1aab3182a
14403 <a7la_soko@yahoo.com>
2023-06-08 10:20:54 -0400

    shalash task is done

---
+++ src/test/java/Campus/POJO_Models/Document.java
@@ -0,0 +1,87 @@
+package Campus.POJO_Models;
+
+public class Document {
+    private String id;
+    private String name;
+    private String attachmentStage;
+    private String description=" ";
+    private boolean required=true;
+    private boolean active=true;
+    private boolean useCamera=false;
+
+    private String[] translateName={};
+    private String schoolId="646cbb07acf2ee0d37c6d984";
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAttachmentStage() {
+        return attachmentStage;
+    }
+
+    public void setAttachmentStage(String attachmentStage) {
+        this.attachmentStage = attachmentStage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isUseCamera() {
+        return useCamera;
+    }
+
+    public void setUseCamera(boolean useCamera) {
+        this.useCamera = useCamera;
+    }
+
+    public String[] getTranslateName() {
+        return translateName;
+    }
+
+    public void setTranslateName(String[] translateName) {
+        this.translateName = translateName;
+    }
+
+    public String getSchoolId() {
+        return schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+}
